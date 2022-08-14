01bdd496a396f577710dc27f2c04d11cfe42e22e
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-14 20:42:21 +0530

    Add getNumberOfLiveNeighbours tests and definition

--- lib/gameoflife.go
+++ lib/gameoflife.go
@@ -20,11 +20,31 @@ func CreateMap(rows, columns int) Map {
 
 func (tempMap *Map) InitialiseGrid(liveCellCount int, liveCells [][]int) {
 	for i := 0; i < liveCellCount; i++ {
-		if liveCells[i][0] < tempMap.rows && liveCells[i][1] < tempMap.columns {
+		if liveCells[i][0] >= 0 && liveCells[i][0] < tempMap.rows && liveCells[i][1] >= 0 && liveCells[i][1] < tempMap.columns {
 			tempMap.Grid[liveCells[i][0]][liveCells[i][1]] = true
 		} else {
 			panic("Index out of range")
 		}
 	}
 	tempMap.liveCells = liveCellCount
+}
+
+func (tempMap Map) getNumberOfLiveNeighbours(row, column int) int {
+	if row < 0 || row >= tempMap.rows || column < 0 || column >= tempMap.columns {
+		panic("Index out of range")
+	}
+	neighbours := 0
+	for i := row - 1; i <= row+1; i++ {
+		for j := column - 1; j <= column+1; j++ {
+			if i > 0 && j > 0 && i < tempMap.rows && j < tempMap.columns {
+				if tempMap.Grid[i][j] == true {
+					neighbours++
+				}
+			}
+		}
+	}
+	if tempMap.Grid[row][column] == true {
+		return neighbours - 1
+	}
+	return neighbours
 }
--- lib/gameoflife_test.go
+++ lib/gameoflife_test.go
@@ -16,7 +16,7 @@ func TestCreateMap(t *testing.T) {
 		})
 	})
 
-	t.Run("check if it does not raise exception when rows or columns passed is non-negative", func(t *testing.T) {
+	t.Run("check if it does not raises exception when rows or columns passed is non-negative", func(t *testing.T) {
 		assert.NotPanics(t, func() {
 			CreateMap(3, 4)
 		})
@@ -33,7 +33,7 @@ func TestCreateMap(t *testing.T) {
 		assert.Equal(t, 2, tempMap.rows)
 	})
 
-	t.Run("check if columns are initialised coreectly", func(t *testing.T) {
+	t.Run("check if columns are initialised correctly", func(t *testing.T) {
 		assert.Equal(t, 3, tempMap.columns)
 	})
 
@@ -64,28 +64,73 @@ func TestInitialiseGrid(t *testing.T) {
 	liveCells[1][0] = 2
 	liveCells[1][1] = 2
 
-	t.Run("check if we can initialise Grid", func(t *testing.T) {
+	t.Run("check if we can initialise grid", func(t *testing.T) {
 		assert.NotPanics(t, func() {
 			tempMap.InitialiseGrid(NumberOfLiveCells, liveCells)
 		})
 	})
 
-	t.Run("check if Grid is properly initialised with passed live cells", func(t *testing.T) {
+	t.Run("check if grid is properly initialised with live cells", func(t *testing.T) {
 		for i := 0; i < NumberOfLiveCells; i++ {
 			assert.Equal(t, true, tempMap.Grid[liveCells[i][0]][liveCells[i][1]])
 		}
 	})
 
+	liveCells[0][0] = 1
+	liveCells[0][1] = -1
 	liveCells[1][0] = 3
 	liveCells[1][1] = 3
 
-	t.Run("check if it panics when index of live cells go out of range", func(t *testing.T) {
+	t.Run("check if it raise exception when index of live cells go out of range", func(t *testing.T) {
 		assert.Panics(t, func() {
 			tempMap.InitialiseGrid(NumberOfLiveCells, liveCells)
 		})
 	})
 
-	t.Run("check if number of live cells is initialised coorectly", func(t *testing.T) {
+	t.Run("check if number of live cells is initialised correctly", func(t *testing.T) {
 		assert.Equal(t, NumberOfLiveCells, tempMap.liveCells)
+	})
+}
+
+func TestGetNumberOfLiveNeighbours(t *testing.T) {
+	rows := 3
+	columns := 3
+	tempMap := CreateMap(rows, columns)
+	NumberOfLiveCells := 2
+	liveCells := make([][]int, NumberOfLiveCells)
+	for i := range liveCells {
+		liveCells[i] = make([]int, 2)
+	}
+	liveCells[0][0] = 1
+	liveCells[0][1] = 2
+	liveCells[1][0] = 2
+	liveCells[1][1] = 2
+	tempMap.InitialiseGrid(NumberOfLiveCells, liveCells)
+
+	t.Run("check if we can get number of neighbours for a cell", func(t *testing.T) {
+		assert.Equal(t, 0, tempMap.getNumberOfLiveNeighbours(0, 0))
+	})
+
+	t.Run("check if it raises exception when row and column passed are out of range", func(t *testing.T) {
+		assert.Panics(t, func() {
+			tempMap.getNumberOfLiveNeighbours(3, 2)
+		})
+		assert.Panics(t, func() {
+			tempMap.getNumberOfLiveNeighbours(0, -2)
+		})
+	})
+
+	t.Run("check if it does not raises exception when row and column passed are in range", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			tempMap.getNumberOfLiveNeighbours(1, 0)
+		})
+	})
+
+	t.Run("check if number of live neighbours are calculated correctly for all edges without raising exception", func(t *testing.T) {
+		assert.Equal(t, 2, tempMap.getNumberOfLiveNeighbours(1, 1))
+		assert.Equal(t, 0, tempMap.getNumberOfLiveNeighbours(2, 0))
+		assert.Equal(t, 1, tempMap.getNumberOfLiveNeighbours(0, 2))
+		assert.Equal(t, 1, tempMap.getNumberOfLiveNeighbours(2, 2))
+		assert.Equal(t, 0, tempMap.getNumberOfLiveNeighbours(0, 0))
 	})
 }
